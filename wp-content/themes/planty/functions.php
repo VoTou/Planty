45e43c12d7d398fa143d6f0158ff63336eaa3054
Noirbenne Valentin <valentin.noirbenne@laposte.net>
2023-04-10 17:17:05 +0200

    Création page contact

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -6,6 +6,8 @@ function theme_enqueue_styles(){
     // Chargement du style.css du thème parent Astra
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
+    // Chargement du css/theme.css pour nos personnalisations
+wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     
 
 
